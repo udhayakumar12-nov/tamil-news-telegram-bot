54f07b964df163fec1cc7ccb7ed380c2e24314c2
udhayakumararuchamy <udhayaram11@gmail.com>
2026-05-11 22:52:03 +0530

    Update news_bot_with_db.py

--- news_bot_with_db.py
+++ news_bot_with_db.py
@@ -18,6 +18,8 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 
 # ---------- CONFIGURATION ----------
+import os
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 BOT_TOKEN = "8553083543:AAFH0mrb0fRgcQTYzDnH0xQPklz_BGmQQcw"
 ADMIN_ID = 8623813419
 
